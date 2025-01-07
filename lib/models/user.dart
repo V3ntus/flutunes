e32a5a662d216564256be43e00278702acd5695c
V3ntus <29584664+V3ntus@users.noreply.github.com>
2025-01-07 00:09:41 -0700

    feat: add jellyfin UserModel

---
+++ lib/models/user.dart
@@ -0,0 +1,37 @@
+class UserModel {
+  final String name;
+  final String serverId;
+  final String serverName;
+  final String primaryImageTag;
+  final String id;
+  final bool enableAutoLogin;
+
+  static UserModel fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      name: json["Name"],
+      serverId: json["ServerId"],
+      serverName: json["ServerName"],
+      primaryImageTag: json["PrimaryImageTag"],
+      id: json["Id"],
+      enableAutoLogin: json["EnableAutoLogin"],
+    );
+  }
+
+  Map<String, dynamic> get json => {
+    "Name": name,
+    "ServerId": serverId,
+    "ServerName": serverName,
+    "PrimaryImageTag": primaryImageTag,
+    "Id": id,
+    "EnableAutoLogin": enableAutoLogin,
+  };
+
+  UserModel({
+    required this.name,
+    required this.serverId,
+    required this.serverName,
+    required this.primaryImageTag,
+    required this.id,
+    required this.enableAutoLogin,
+  });
+}

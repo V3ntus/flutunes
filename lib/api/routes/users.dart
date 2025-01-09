46f078cf4d042cd22ff60ff5c94f2558506678fa
V3ntus <29584664+V3ntus@users.noreply.github.com>
2025-01-08 19:16:28 -0700

    ref: add user state shared pref keys

--- lib/api/routes/users.dart
+++ lib/api/routes/users.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutunes/api/routes/_base.dart';
 import 'package:flutunes/models/user.dart';
+import 'package:flutunes/shared_preferences.dart';
 
 class IncorrectCredentialsError extends Error {
   @override
@@ -13,22 +14,27 @@ class Users extends BaseRoute {
   Users(super.client);
 
   Future<UserModel> authenticateByName(String username, String password) async {
-    client.currentUser =
-        UserModel(name: username, id: "", enableAutoLogin: false);
-    final Response response = await client.request("POST",
-        (await client.baseUri()).replace(path: "/Users/AuthenticateByName"),
-        json: {
-          "Username": username,
-          "Pw": password,
-        });
+    client.currentUser = UserModel(name: username, id: "", enableAutoLogin: false);
+    final Response response = await client.request(
+      "POST",
+      (await client.baseUri()).replace(path: "/Users/AuthenticateByName"),
+      json: {
+        "Username": username,
+        "Pw": password,
+      },
+    );
 
-    if (response.statusCode == 401) {
+    if (response.statusCode == 401 || response.data["AccessToken"] == null) {
       client.currentUser = null;
       throw IncorrectCredentialsError();
     }
 
     client.accessToken = response.data["AccessToken"];
+    await asyncPrefs.setString(PreferenceKeys.ACCESS_TOKEN.key, client.accessToken!);
+
     final authUser = UserModel.fromJson(response.data["User"]);
+    await asyncPrefs.setString(PreferenceKeys.LAST_LOGGED_IN_USERNAME.key, authUser.name);
+    await asyncPrefs.setBool(PreferenceKeys.SHOULD_AUTO_LOGIN.key, authUser.enableAutoLogin);
 
     client.currentUser = authUser;
     return authUser;
--- lib/shared_preferences.dart
+++ lib/shared_preferences.dart
@@ -2,11 +2,15 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 enum PreferenceKeys {
-  SERVER_URL("SERVER_URL");
+  SERVER_URL(String, "SERVER_URL"),
+  ACCESS_TOKEN(String, "ACCESS_TOKEN"),
+  LAST_LOGGED_IN_USERNAME(String, "LAST_LOGGED_IN_USERNAME"),
+  SHOULD_AUTO_LOGIN(bool, "SHOULD_AUTO_LOGIN");
 
+  final Type type;
   final String key;
 
-  const PreferenceKeys(this.key);
+  const PreferenceKeys(this.type, this.key);
 
   @override
   String toString() {

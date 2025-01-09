32195da14281c3cc5280cc954ccdabe4708a93f4
V3ntus <29584664+V3ntus@users.noreply.github.com>
2025-01-08 18:17:36 -0700

    feat: splash screen route

---
+++ lib/routes/splash.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _checkIfLoggedIn();
+  }
+
+  Future<void> _checkIfLoggedIn() async {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Flutunes",
+          style: Theme.of(context).textTheme.displayLarge,
+        ),
+      ),
+    );
+  }
+}

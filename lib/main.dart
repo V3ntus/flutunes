1e200505ababbb5e850c76b3a8ef2eb239cdbc33
V3ntus <29584664+V3ntus@users.noreply.github.com>
2025-01-08 18:18:28 -0700

    ref: implement go_router into app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutunes/routes/login.dart';
+import 'package:flutunes/routes/splash.dart';
+import 'package:go_router/go_router.dart';
+
+final _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: "/login",
+      builder: (_, __) => LoginScreen(),
+    ),
+    GoRoute(
+      path: "/",
+      builder: (_, __) => SplashScreen(),
+    ),
+  ],
+);
 
 void main() {
   runApp(const App());
@@ -10,41 +26,19 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Flutunes',
       theme: ThemeData.dark(useMaterial3: true).copyWith(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const Homepage(title: 'Flutunes'),
-    );
-  }
-}
-
-class Homepage extends StatefulWidget {
-  const Homepage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<Homepage> createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Home"),
-          ],
+        inputDecorationTheme: InputDecorationTheme(
+          contentPadding: EdgeInsets.all(20),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(24),
+            gapPadding: 8.0,
+          ),
         ),
       ),
+      routerConfig: _router,
     );
   }
 }
